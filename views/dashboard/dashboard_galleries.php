b1d7a3f94e7f12a13abf57bcbadb666171c19ba5
NathSourire <134375430+NathSourire@users.noreply.github.com>
2023-11-26 10:34:00 +0100

    1

--- views/dashboard/dashboard_galleries.php
+++ views/dashboard/dashboard_galleries.php
@@ -8,7 +8,7 @@
                     <th class=" bg-transparent ">Nom du produit</th>
                     <th class=" bg-transparent ">Photo</th>
                     <th class=" bg-transparent ">Archivé le </th>
-                    <th class=" bg-transparent ">Modifier</th>
+                    <!-- <th class=" bg-transparent ">Modifier</th> -->
                     <th class=" bg-transparent ">Archiver</th>
                     <th class=" bg-transparent ">Restorer</th>
                 </thead>
@@ -22,10 +22,10 @@
                                 <td class=" bg-transparent "><a href="/public/uploads/image/<?= $image->image ?>" target="_blank"><?= $image->image ?></a></td>
                             <?php } ?>
                             <td class=" bg-transparent "><?= $image->archived_at ?></td>
-                            <td class=" bg-transparent "><a href="/controllers/dashboard/dashboard_galleries_ctrl.php?id_galleries=<?= $image->id_galleries ?>">
+                            <!-- <td class=" bg-transparent "><a href="/controllers/dashboard/dashboard_galleries_ctrl.php?id_galleries=<?= $image->id_galleries ?>">
                                     <img src="/public/assets/img/btnwrite.png" alt="stylo">
                                 </a>
-                            </td>
+                            </td> -->
                             <td class=" bg-transparent "><a href="/controllers/dashboard/dashboard_galleries_ctrl.php?action=archive&id_galleries=<?= $image->id_galleries ?>">
                                     <img src="/public/assets/img/btnarchived.png" alt="archive">
                                 </a>
